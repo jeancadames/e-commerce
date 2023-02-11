8c30c3416703663b7112b3cb2dca2e6106a16be8
Jean <jeancadames22@gmail.com>
2023-02-11 17:17:59 -0400

    image srcs change

--- src/components/ContentImage.jsx
+++ src/components/ContentImage.jsx
@@ -10,19 +10,19 @@ export const ContentImage = () => {
             <div className="images-container">
                 <Link to={'Basketball'}>
                 <div className="basketball-bg">
-                    <img src="src\assets\basketball bg.avif" alt="basketball-image" />
+                    <img src="https://images.unsplash.com/photo-1608245449230-4ac19066d2d0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="basketball-image" />
                     <h2>Explosiveness</h2>
                 </div>
                 </Link>
                 <Link to={'/Boxing'}>
                 <div className="boxing-bg">
-                    <img src="src\assets\boxing bg.avif" alt="boxing-image" />
+                    <img src="https://images.unsplash.com/photo-1606335543042-57c525922933?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1175&q=80" alt="boxing-image" />
                     <h2>Strength</h2>
                 </div>
                 </Link>
                 <Link to={'/Tennis'}>
                 <div className="tennis-bg">
-                    <img src="src\assets\tennis bg.avif" alt="tensis-image" />
+                    <img src="https://images.unsplash.com/photo-1583275478661-1c31970669fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="tensis-image" />
                     <h2>Precision</h2>
                 </div>
                 </Link>
